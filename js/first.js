1cf0cc746d2f5c951d59f625963cd2d11d5c955a
rithi <rithisumaiya.11.11@gmail.com>
2024-02-20 17:32:22 +0600

    unnecessary space removed

--- js/first.js
+++ js/first.js
@@ -4,40 +4,48 @@ const classList = document.getElementById('class');
 const priceList = document.getElementById('price');
 const supTag = document.getElementById('seat-count');
 const seatsLeftSpan = document.getElementById('seats-left');
-const totalPriceSpan = document.getElementById('total-price'); 
+const totalPriceSpan = document.getElementById('total-price');
 const grandTotalSpan = document.getElementById('grand-total');
 
 document.addEventListener('click', 
-function(event) {
-    if (event.target.classList.contains('seat')) {
+function(event){
+    if(event.target.classList.contains('seat')){
         const seatId = event.target.id;
         const seat = document.getElementById(seatId);
-        if (!seat.classList.contains('disabled') && selectedSeatsCount < 4) { 
+        if(!seat.classList.contains('disabled') && selectedSeatsCount < 4){
             seat.style.color = 'white';
             seat.style.backgroundColor = '#1DD100';
             seat.classList.add('disabled');
             seat.classList.remove('opacity-50');
             selectedSeatsCount++;
+
             const selectedSeatItem = document.createElement('li');
             selectedSeatItem.textContent = seatId;
             selectedSeatsList.appendChild(selectedSeatItem);
+
             const classItem = document.createElement('li');
             classItem.textContent = 'Economy';
             classList.appendChild(classItem);
+
             const priceItem = document.createElement('li');
             priceItem.textContent = '550';
             priceList.appendChild(priceItem);
+
             const currentCount = parseInt(supTag.textContent);
             supTag.textContent = currentCount + 1;
+
             const seatsLeft = parseInt(seatsLeftSpan.textContent);
             seatsLeftSpan.textContent = seatsLeft - 1;
+
             const currentTotalPrice = parseInt(totalPriceSpan.textContent);
             totalPriceSpan.textContent = currentTotalPrice + 550;
             grandTotalSpan.textContent = currentTotalPrice + 550;
-        } else if (selectedSeatsCount >= 4) {
-            alert('You can only select up to 4 seats.');
-        } else {
-            alert('Seat ' + seatId + ' is already selected.');
+        }
+        else if(selectedSeatsCount >= 4){
+            alert('Not more than four seats can be booked.');
+        }
+        else{
+            alert(seatId + ' is already booked.');
         }
     }
 });
@@ -47,21 +55,23 @@ const applyButton = document.getElementById('apply-button');
 const couponDiv = document.getElementById('coupon-div');
 
 couponInput.addEventListener('input', 
-function() {
+function(){
     const couponCode = couponInput.value;
     const validCouponCodes = ['NEW15', 'Couple 20'];
-    
-    if (!validCouponCodes.includes(couponCode)) {
-        document.getElementById('coupon-message').textContent = 'Please provide a valid coupon code';
-    } else {
+
+    if(!validCouponCodes.includes(couponCode)){
+        document.getElementById('coupon-message').textContent = 'Please enter a valid coupon code';
+    }
+    else{
         document.getElementById('coupon-message').textContent = '';
     }
 
-    if (selectedSeatsCount === 4 && (couponCode === 'NEW15' || couponCode === 'Couple 20')) {
+    if(selectedSeatsCount === 4 && (couponCode === 'NEW15' || couponCode === 'Couple 20')){
         applyButton.disabled = false;
         applyButton.classList.remove('disabled');
         applyButton.style.backgroundColor = '#27AE60';
-    } else {
+    }
+    else{
         applyButton.disabled = true;
         applyButton.classList.add('disabled');
         applyButton.style.backgroundColor = '#1DD100';
@@ -69,63 +79,71 @@ function() {
 });
 
 applyButton.addEventListener('click', 
-function() {
+function(){
     const couponCode = couponInput.value;
     let discountPercentage = 0;
-    if (couponCode === 'NEW15') {
+
+    if(couponCode === 'NEW15'){
         discountPercentage = 0.15;
-    } else if (couponCode === 'Couple 20') {
+    }
+    else if(couponCode === 'Couple 20'){
         discountPercentage = 0.20;
     }
-    if (discountPercentage > 0) {
+    if(discountPercentage > 0){
         const discount = parseInt(totalPriceSpan.textContent) * discountPercentage;
         const grandTotal = parseInt(totalPriceSpan.textContent) - discount;
+
         grandTotalSpan.textContent = grandTotal;
         couponDiv.style.display = 'none';
         appendDiscountItem('Discount', discount);
     }
 });
 
-function appendDiscountItem(discountText, discountValue) {
+function appendDiscountItem(discountText, discountValue){
     const priceHeadingList = document.getElementById('price-heading');
     const discountItem = document.createElement('li');
+
     discountItem.textContent = discountText;
     priceHeadingList.appendChild(discountItem);
 
     const priceDiscountAddList = document.getElementById('price-discount-add');
     const discountValueItem = document.createElement('li');
+
     discountValueItem.textContent = 'BDT ' + discountValue;
     priceDiscountAddList.appendChild(discountValueItem);
 }
-
 
 const phoneNumberInput = document.getElementById('phone-number-input');
 const nextButton = document.getElementById('next-button');
 
 phoneNumberInput.addEventListener('input', 
-function() {
+function(){
     checkInputs();
 });
+
 document.addEventListener('click', 
-function(event) {
-    if (event.target.classList.contains('seat')) {
+function(event){
+    if(event.target.classList.contains('seat')){
         checkInputs();
     }
 });
-function checkInputs() {
+
+function checkInputs(){
     const selectedSeatsCount = document.querySelectorAll('.seat.disabled').length;
     const phoneNumber = phoneNumberInput.value;
 
-    if (selectedSeatsCount >= 1 && phoneNumber !== '') {
+    if(selectedSeatsCount >= 1 && phoneNumber !== ''){
         nextButton.disabled = false;
         nextButton.style.backgroundColor = '#27AE60';
-    } else {
+    }
+    else{
         nextButton.disabled = true;
         nextButton.style.backgroundColor = '#1DD100';
     }
+
     nextButton.addEventListener('click', 
-    function() {
-        if (selectedSeatsCount >= 1 && phoneNumber !== '') {
+    function(){
+        if(selectedSeatsCount >= 1 && phoneNumber !== ''){
             document.querySelector('.seat-selection-section').style.display = 'none';
             document.querySelector('.success-message-section').style.display = 'block';
         }
